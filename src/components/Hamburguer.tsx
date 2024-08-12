685908c3cf0e2b901b0426800d715bdea0bbc256
4llves <jhonatab4@gmail.com>
2024-08-12 20:22:22 -0300

    feat: add font menu and style

--- src/components/Hamburguer.tsx
+++ src/components/Hamburguer.tsx
@@ -1,11 +1,19 @@
 'use client'
 import clsx from 'clsx'
 import { useState } from 'react'
+import Logo from '../assets/sliceLogo.svg'
+import Image from 'next/image'
+import { Quantico } from 'next/font/google'
+
+const quantico = Quantico({
+  subsets: ['latin'],
+  weight: '700',
+})
 
 export function Hamburguer() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
-  const menuItemClassName = clsx('md:block md:text-lg text-4xl', {
+  const menuItemClassName = clsx('md:block md:text-xl text-xl group', {
     visible: mobileMenuOpen,
     hidden: !mobileMenuOpen,
   })
@@ -14,16 +22,55 @@ export function Hamburguer() {
     <div>
       <header
         className={clsx(
-          'bg-black text-white p-8 flex flex-col md:flex-row gap-6',
+          `bg-black md:flex md:items-center md:justify-center text-green-titles p-8 flex flex-col md:flex-row gap-10 cursor-pointer ${quantico.className}`,
           {
             'h-screen': mobileMenuOpen,
           },
         )}
       >
-        <a className={menuItemClassName}>Home</a>
-        <a className={menuItemClassName}>Quem somos nós</a>
-        <a className={menuItemClassName}>Serviços</a>
-        <a className={menuItemClassName}>Contatos</a>
+        <a className={menuItemClassName}>
+          <Image src={Logo} alt="" width={30} height={30}></Image>
+        </a>
+        <a className={menuItemClassName}>
+          <strong className="text-green-titles md:group-hover:text-green-titles md:text-transparent">
+            &lt;{' '}
+          </strong>
+          Home
+          <strong className="text-green-titles md:group-hover:text-green-titles md:text-transparent">
+            {' '}
+            /&gt;
+          </strong>
+        </a>
+        <a className={menuItemClassName}>
+          <strong className="text-green-titles md:group-hover:text-green-titles md:text-transparent">
+            &lt;{' '}
+          </strong>
+          Sobre
+          <strong className="text-green-titles md:group-hover:text-green-titles md:text-transparent">
+            {' '}
+            /&gt;
+          </strong>
+        </a>
+        <a className={menuItemClassName}>
+          <strong className="text-green-titles md:group-hover:text-green-titles md:text-transparent">
+            &lt;{' '}
+          </strong>
+          Projetos
+          <strong className="text-green-titles md:group-hover:text-green-titles md:text-transparent">
+            {' '}
+            /&gt;
+          </strong>
+        </a>
+        <a className={menuItemClassName}>
+          <strong className="text-green-titles md:group-hover:text-green-titles md:text-transparent">
+            &lt;{' '}
+          </strong>
+          Talks
+          <strong className="text-green-titles md:group-hover:text-green-titles md:text-transparent">
+            {' '}
+            /&gt;
+          </strong>
+        </a>
 
         <button
           className="md:hidden absolute right-4 top-6"
