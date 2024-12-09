68302f30de77f8184849fef404cc972b5a7908ae
4llves <jhonatab4@gmail.com>
2024-12-09 02:42:20 -0300

    Adds click handler to menu links

--- src/components/hamburguer-menu.tsx
+++ src/components/hamburguer-menu.tsx
@@ -36,7 +36,7 @@ export function HamburguerMenu() {
           }
         )}
       >
-        <Link href="/" className={menuItemClassName}>
+        <Link href="/" className={menuItemClassName} onClick={handleLinkClick}>
           {mobileMenuOpen ? (
             <div className="flex items-center gap-4">
               <AllvesLogo /> 4llves dev
